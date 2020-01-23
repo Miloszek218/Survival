4add657a1e7c7e28b34b682ec04213f4f2510084
Miloszek218 <33146324+Miloszek218@users.noreply.github.com>
2020-01-24 00:18:27 +0100

    Nowe na dzien 24

---
+++ Assets/vIHealthController.cs
@@ -0,0 +1,22 @@
+﻿using Invector;
+
+public partial class TigerAi
+{
+    public TigerAi()
+    {
+    }
+
+    public interface vIHealthController : vIDamageReceiver
+    {
+        OnDead onDead { get; }
+        float currentHealth { get; }
+        int MaxHealth { get; }
+        bool isDead { get; set; }
+        void ChangeHealth(int value);
+        void ChangeMaxHealth(int value);
+    }
+                return;
+
+        }
+    }
+}
